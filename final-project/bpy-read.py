d18e7d4d1345afaaa5021aad5058f8dc2b5eba62
Don Athalage <dknathalage@gmail.com>
2022-05-11 09:53:35 +1000

    read-data

---
+++ final-project/bpy-read.py
@@ -0,0 +1,8 @@
+from bluepy.btle import UUID, Peripheral
+
+p = Peripheral("3C:61:05:3F:CA:6A", "public")
+
+dataService = p.getServiceByUUID("19b10000-e8f2-537e-4f6c-d104768a1214")
+
+for ch in dataService.getCharacteristics():
+    print(ch.read())
--- final-project/rpi/bpy-scan.py
+++ final-project/rpi/bpy-scan.py
@@ -2,6 +2,8 @@ from bluepy.btle import UUID, Peripheral
 
 p = Peripheral("3C:61:05:3F:CA:6A", "public")
 
+print("Handle   UUID                                Properties")
+print("-------------------------------------------------------")
 for ch in p.getCharacteristics():
     print("  0x" + format(ch.getHandle(), '02X') + "   " +
           str(ch.uuid) + " " + ch.propertiesToString())
