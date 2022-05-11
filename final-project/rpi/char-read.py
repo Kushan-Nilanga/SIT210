1a2e036ccbbe654066ff4a06228855b820bc8d34
Don Athalage <dknathalage@gmail.com>
2022-05-11 10:19:37 +1000

    minor-changes

--- final-project/rpi/char-read.py
+++ final-project/rpi/char-read.py
@@ -6,4 +6,4 @@ p = Peripheral("3C:61:05:3F:CA:6A", "public")
 dataService = p.getServiceByUUID("19b10000-e8f2-537e-4f6c-d104768a1214")
 
 for ch in dataService.getCharacteristics():
-    print(struct.unpack('f', ch.read()))
+    print(ch.uuid, struct.unpack('f', ch.read()))
