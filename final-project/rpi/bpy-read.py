5b359217dcf05fbf136f5306318a53dd6052603f
Don Athalage <dknathalage@gmail.com>
2022-05-11 10:08:05 +1000

    unpack

--- final-project/rpi/bpy-read.py
+++ final-project/rpi/bpy-read.py
@@ -1,8 +1,9 @@
 from bluepy.btle import UUID, Peripheral
+import struct
 
 p = Peripheral("3C:61:05:3F:CA:6A", "public")
 
 dataService = p.getServiceByUUID("19b10000-e8f2-537e-4f6c-d104768a1214")
 
 for ch in dataService.getCharacteristics():
-    print(ch.read())
+    print(struct.unpack(ch.read()))
