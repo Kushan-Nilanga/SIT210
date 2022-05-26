8719861b286363769a283f29212b5a20ca9d61fd
Kushan-Nilanga <dknathalage@gmail.com>
2022-05-26 04:24:23 +0100

    changes

--- final-project/rpi/char-read.py
+++ final-project/rpi/char-read.py
@@ -1,7 +1,7 @@
 from bluepy.btle import UUID, Peripheral
 import struct
 
-p = Peripheral("3C:61:05:3F:CA:6A", "public")
+p = Peripheral("E5:D0:E2:D6:80:E1", "random")
 
 dataService = p.getServiceByUUID("19b10000-e8f2-537e-4f6c-d104768a1214")
 
--- final-project/rpi/main.py
+++ final-project/rpi/main.py
@@ -9,10 +9,9 @@ from datetime import datetime
 devices = []
 dataServiceUUID = "19b10000-e8f2-537e-4f6c-d104768a1214"
 
-
 def get_device_data(device):
     try:
-        peripheral = Peripheral(device)
+        peripheral = Peripheral(device, "random")
         dataService = peripheral.getServiceByUUID(dataServiceUUID)
         data = {}
         data["time"] = str(datetime.now())
@@ -22,15 +21,14 @@ def get_device_data(device):
         peripheral.disconnect()
         return data
     except:
-        print("Error fetching data via BLE")
+        print(str(datetime.now()), "BLE connection err")
         return None
-
 
 def get_device_uuids(url):
     try:
         return get(url).json()
     except:
-        print("UUID fetch failed")
+        print(str(datetime.now()), "UUID fetch failed")
         return []
 
 
@@ -40,21 +38,22 @@ def post_device_data(url, device, data):
 
     try:
         post(url, {"uuid": device, "data": dumps(data)})
-        print("data posted")
+        print(str(datetime.now()), "data posted")
     except:
-        print("data post to failed")
+        print(str(datetime.now()), "data post to failed")
 
 
 def loop():
     devices = get_device_uuids("http://192.168.0.104:3000/api/container/uuids")
     for device in devices:
         data = get_device_data(device)
-        post_device_data(
+        if data is not None:    
+            post_device_data(
             "http://192.168.0.104:3000/api/container/log",
             device,
             data)
 
-    sleep(10)
+    sleep(60)
 
 
 if __name__ == "__main__":
